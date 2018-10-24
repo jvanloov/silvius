bc140247ee4dd619784d7bf3264c59a9e2ade386
Joris Van Looveren <joris@van-looveren.net>
2018-10-24 21:08:08 +0200

    Add 'back'/'scroll' (pgup/pgdn), and 'jump left/jump right' (esc-left, esc-right)

--- grammar/grammars/movement_editing.py
+++ grammar/grammars/movement_editing.py
@@ -1,4 +1,5 @@
 from ast import AST
+from scan import Token
 
 class MovementEditingMixin(object):
 
@@ -12,13 +13,44 @@ class MovementEditingMixin(object):
             movement ::= down   repeat
             movement ::= left   repeat
             movement ::= right  repeat
+            movement ::= back   repeat
+            movement ::= scroll repeat
+            movement ::= jump_cmd left  repeat
+            movement ::= jump_cmd right repeat
         '''
-        if args[1] != None:
+        if args[0].type == 'jump':
+            # TODO: this is Emacs specific; refactor?
+            movement = AST('chain', None, [
+                AST("raw_char", [ 'Escape' ] ),
+                AST("movement", [ Token(args[1].type) ] )
+                ])
+            if len(args) > 2 and args[2] != None:
+                args[0] = AST('repeat', [ args[2] ], [ movement ] )
+            else:
+                args[0] = movement
+            return args[0]
+        elif args[1] != None:
+            movement = args[0]
+            if movement.type == "back":
+                movement = Token("pageup")
+            elif movement.type == "scroll":
+                movement = Token("pagedown")
             return AST('repeat', [ args[1] ], [
-                AST('movement', [ args[0] ])
+                AST('movement', [ movement ])
             ])
         else:
-            return AST('movement', [ args[0] ])
+            movement = args[0]
+            if movement.type == "back":
+                movement = Token("pageup")
+            elif movement.type == "scroll":
+                movement = Token("pagedown")
+            return AST('movement', [ movement ])
+
+    def p_jump_cmd(self, args):
+        '''
+            jump_cmd ::= jump
+        '''
+        return AST('jump')
 
     def p_editing(self, args):
         '''
